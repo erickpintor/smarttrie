cd6704f2459ff65c3f0b1b19eeac940e32308e00
Erick Pintor <erickpintor@gmail.com>
2020-09-29 10:53:27 -0300

    adding YCSB client

--- src/main/scala/smarttrie/app/client/ClientApp.scala
+++ src/main/scala/smarttrie/app/client/ClientApp.scala
@@ -1,21 +1,105 @@
 package smarttrie.app.client
 
 import bftsmart.tom.ServiceProxy
+import io.netty.buffer.Unpooled
+import java.util.concurrent.ConcurrentHashMap
+import java.util.logging.{Level, Logger}
+import java.util.{HashMap => JHashMap, Map => JMap, Set => JSet, Vector => JVector}
+import scala.jdk.CollectionConverters._
+import scala.util.control.NonFatal
+import site.ycsb.{ByteArrayByteIterator, ByteIterator, DB, Status}
 import smarttrie.atoms._
 import smarttrie.io._
 import smarttrie.lang._
 
-object ClientApp extends App {
+final class ClientApp extends DB {
   import Command._
+  import Reply._
 
-  val proxy = new ServiceProxy(2)
-  val set = Set(Key("foo".toBuf), Value("bar".toBuf))
-  val reply0 = proxy.invokeOrdered(Codec.encode(set).toByteArray)
-  println("------------------------------------------")
-  println(Codec.decode(reply0).as[Reply])
+  private[this] val logger = Logger.getLogger("client")
+  private[this] val cache = new ConcurrentHashMap[String, Key]()
+  private[this] var proxy: ServiceProxy = _
 
-  val get = Get(Key("foo".toBuf))
-  val reply1 = proxy.invokeOrdered(Codec.encode(get).toByteArray)
-  println("------------------------------------------")
-  println(Codec.decode(reply1).as[Reply])
+  override def init(): Unit = {
+    val serverID = getProperties.getProperty("serverID")
+    if (serverID eq null) {
+      throw new IllegalStateException("Missing server id")
+    }
+    proxy = new ServiceProxy(serverID.toInt)
+  }
+
+  def insert(
+      table: String,
+      key: String,
+      values: JMap[String, ByteIterator]
+  ): Status = update(table, key, values)
+
+  def update(
+      table: String,
+      key: String,
+      values: JMap[String, ByteIterator]
+  ): Status = {
+    val k = cachedKey(key)
+    val v = singleValue(values)
+    val res = execute(Set(k, v))
+    toStatus(res)
+  }
+
+  def delete(table: String, key: String): Status = {
+    val k = cachedKey(key)
+    val res = execute(Remove(k))
+    toStatus(res)
+  }
+
+  def read(
+      table: String,
+      key: String,
+      fields: JSet[String],
+      result: JMap[String, ByteIterator]
+  ): Status = {
+    val k = cachedKey(key)
+    execute(Get(k)) match {
+      case Null  => Status.OK
+      case Error => Status.ERROR
+      case Data(value) =>
+        val bytes = new ByteArrayByteIterator(value.buf.toByteArray)
+        result.put("field0", bytes)
+        Status.OK
+    }
+  }
+
+  def scan(
+      table: String,
+      startKey: String,
+      recordCount: Int,
+      fields: JSet[String],
+      results: JVector[JHashMap[String, ByteIterator]]
+  ): Status =
+    throw new UnsupportedOperationException
+
+  private def cachedKey(key: String): Key =
+    cache.computeIfAbsent(key, (str: String) => Key(str.toBuf))
+
+  private def singleValue(values: JMap[String, ByteIterator]): Value = {
+    val (_, bytes) = values.asScala.head
+    Value(Unpooled.wrappedBuffer(bytes.toArray))
+  }
+
+  private def toStatus(reply: Reply): Status =
+    reply match {
+      case Null | Data(_) => Status.OK
+      case Error          => Status.ERROR
+    }
+
+  private def execute(cmd: Command): Reply =
+    try {
+      val msg = Codec.encode(cmd)
+      val res = proxy.invokeUnordered(msg.toByteArray)
+      Codec.decode(res).as[Reply]
+    } catch {
+      case NonFatal(err) =>
+        logger.log(Level.SEVERE, "Request failed", err)
+        Error
+    }
+
 }
--- src/main/scala/smarttrie/app/server/ServerApp.scala
+++ src/main/scala/smarttrie/app/server/ServerApp.scala
@@ -1,10 +1,8 @@
 package smarttrie.app.server
 
 import bftsmart.tom.ServiceReplica
-import java.nio.file.{Files, Paths}
 
 object ServerApp extends App {
-  Files.deleteIfExists(Paths.get("./config/currentView"))
   val server = new KeyValueServer(State.hashMap)
   new ServiceReplica(args(0).toInt, server, server)
 }
