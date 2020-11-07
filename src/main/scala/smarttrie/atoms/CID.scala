1ceaa8570b30e7c470096c5eede74ba3e22769da
Erick Pintor <erickpintor@gmail.com>
2020-11-07 10:17:32 -0300

    remove unnecessary next from CID

--- src/main/scala/smarttrie/atoms/CID.scala
+++ src/main/scala/smarttrie/atoms/CID.scala
@@ -4,12 +4,12 @@ import smarttrie.io._
 
 final case class CID(toInt: Int) extends AnyVal with Ordered[CID] {
   @inline def compare(that: CID): Int = toInt.compareTo(that.toInt)
-  @inline def next: CID = CID(toInt + 1)
   @inline override def toString: String = s"CID($toInt)"
 }
 
 object CID {
   val Null = CID(-1)
+
   implicit object CIDCodec extends Codec[CID] {
     def encode(cid: CID, out: Encoder.Output): Unit = out.writeInt(cid.toInt)
     def decode(in: Decoder.Input): CID = CID(in.readInt)
